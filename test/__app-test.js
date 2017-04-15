b1aa6a3e4cef94db7e9036fef5ad19d8b58b6e76
r3b311i0n <nasashinega@gmail.com>
2017-04-15 17:53:46 +0530

    Had both tslint@5.0.0 and ts-lint@4.5.1. Removed ts-lint.

--- test/__app-test.js
+++ test/__app-test.js
@@ -1,13 +1,11 @@
 const assert = require('assert');
 
-const App = require('../src/app/app.component');
+const app = require('../src/app/app.component');
 
 describe('#App', () => {
     describe('#karmaTester()', () => {
         it('should work', () => {
-            const app = new App.App({});
-
-            assert.equal(0, app.karmaTester());
+            assert.equal(0, app.App.karmaTester());
         })
     });
 });
