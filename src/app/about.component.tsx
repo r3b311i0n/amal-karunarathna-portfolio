baefc6fcc49f24df50da0fb8b0d3cebfc40100fd
r3b311i0n <nasashinega@gmail.com>
2017-04-25 21:05:30 +0530

    Separated my description into a different component.

--- src/app/about.component.tsx
+++ src/app/about.component.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import {Motion, spring} from 'react-motion';
 import './about.component.css';
+import AboutMe from './stateless-components/about-me';
 import NavBar from './stateless-components/nav-bar';
 
 const active = {color: 'rgba(0, 0, 0, 1)', 'font-size': '1.333em'};
 const inactive = {color: 'inherit', 'font-size': '1em'};
 
-export class About extends React.Component<void, any> {
+interface IAboutState {
+    willSlideIn: boolean;
+}
+
+export class About extends React.Component<void, IAboutState> {
     constructor(props: any) {
         super(props);
 
@@ -36,27 +41,7 @@ export class About extends React.Component<void, any> {
                             {(interpolation: any) => <div
                                 style={{transform: `translateX(${interpolation.x}px)`, opacity: interpolation.alpha}}
                             >
-                                <section className="about-me">
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores
-                                        autem
-                                        beatae blanditiis delectus eveniet facilis hic labore, laudantium natus non
-                                        numquam
-                                        obcaecati pariatur perspiciatis placeat quisquam repellendus sapiente, sunt.
-                                    </p>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores
-                                        aspernatur consequuntur cum deserunt exercitationem illo labore laudantium
-                                        mollitia
-                                        natus nemo neque nostrum nulla odio omnis, provident quo rerum voluptates.
-                                    </p>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae doloremque,
-                                        id
-                                        inventore ipsa libero natus nesciunt, obcaecati qui quibusdam quo rem,
-                                        reprehenderit
-                                        soluta tempora temporibus. Dolore ea tenetur voluptas.
-                                    </p>
-                                </section>
+                                <AboutMe/>
                             </div>}
                         </Motion>
                     </main>
---
+++ src/app/stateless-components/about-me.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import './about-me.css';
+
+// My stateless component.
+
+const AboutMe = () => (
+    <section className="about-me">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores
+            autem
+            beatae blanditiis delectus eveniet facilis hic labore, laudantium natus non
+            numquam
+            obcaecati pariatur perspiciatis placeat quisquam repellendus sapiente, sunt.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores
+            aspernatur consequuntur cum deserunt exercitationem illo labore laudantium
+            mollitia
+            natus nemo neque nostrum nulla odio omnis, provident quo rerum voluptates.
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab beatae doloremque,
+            id
+            inventore ipsa libero natus nesciunt, obcaecati qui quibusdam quo rem,
+            reprehenderit
+            soluta tempora temporibus. Dolore ea tenetur voluptas.
+        </p>
+    </section>
+);
+
+export default AboutMe;
