619fb805b1702f0988cf5cb579c5e4e7a8ab4dc3
r3b311i0n <nasashinega@gmail.com>
2017-05-12 03:14:46 +0530

    Remove active styles for tags and put original code in __tag-forSort.

---
+++ src/app/stateless-components/__tag-forSort.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import './__tag-forSort.pcss';
+
+const Tag = (tagName: string) => {
+    switch (tagName) {
+        case 'drugs':
+            return (
+                <div className="tag-root yellow" key={tagName}>
+                    {tagName}
+                </div>
+            );
+        case 'meth':
+            return (
+                <div className="tag-root orange" key={tagName}>
+                    {tagName}
+                </div>
+            );
+        case 'poverty':
+            return (
+                <div className="tag-root turquoise" key={tagName}>
+                    {tagName}
+                </div>
+            );
+        case 'white trash':
+            return (
+                <div className="tag-root purple" key={tagName}>
+                    {tagName}
+                </div>
+            );
+        default:
+            return (
+                <div className="tag-root" key={tagName}>
+                    {tagName}
+                </div>
+            );
+    }
+};
+
+export default Tag;
