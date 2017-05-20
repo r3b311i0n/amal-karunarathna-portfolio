09d19a3d22872ad2c087ffe23baaa49e57797572
r3b311i0n <nasashinega@gmail.com>
2017-05-20 11:27:11 +0530

    Clean up and refactor members to static members.

--- src/app/blog.component.tsx
+++ src/app/blog.component.tsx
@@ -6,7 +6,6 @@ import './blog.component.pcss';
 import BlogLink from './stateless-components/blog-link';
 import NavMenu from './stateless-components/nav-menu';
 
-// todo: Scrollable blog link list.
 // todo: Put all constants in in their own file.
 // todo: shrink Blog List for small screens.
 // todo: Animate heading separately.
@@ -17,22 +16,6 @@ const inactive = {color: 'inherit', fontSize: '1em'};
 
 const database = firebase.database();
 
-const cocaineArticle = {
-    body: {
-        para1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci amet corporis distinctio error itaque laboriosam molestias mollitia nisi officia perferendis placeat, quae quia quod, ratione reiciendis sit veniam? Quasi?'
-    },
-    header: 'Coke is love, Coke is life!'
-};
-
-const md: string = 'An h1 header ============ Paragraphs are separated by a blank line. 2nd paragraph. *Italic*, **bold**, and `monospace`. Itemized list look like: * this one * that one * the other oneNote that --- not considering the asterisk --- the actual textcontent starts at 4-columns in. > Block quotes are > written like so. > > They can span multiple paragraphs, > if you like.Use 3 dashes for an em-dash. Use 2 dashes for ranges (ex., "it\'s all in chapters 12--14"). Three dots ... will be converted to an ellipsis Unicode is supported. ☺ <img src=https://www.slt.lk//sites/default/files/landing_page_banners/Mega-Web-Banner2_0_0.jpg>';
-
-const methArticle = {
-    body: {
-        para1: md
-    },
-    header: 'Meth is the Greatest Thing Ever!'
-};
-
 const staggerStyles = (prevInterpolatedStyles: any) => prevInterpolatedStyles.map((_: { h: number }, i: number) => {
     return i === 0
         ? {h: spring(0, presets.stiff)}
@@ -40,10 +23,10 @@ const staggerStyles = (prevInterpolatedStyles: any) => prevInterpolatedStyles.ma
 });
 
 interface IBlogState {
-    article: string;
+    articleBody: string;
     articleDefaultStyle: any;
     articleStyle: any;
-    header: string;
+    articleHeading: string;
     willAnimateInBlogLinkList: boolean;
 }
 
@@ -51,26 +34,25 @@ export class Blog extends React.Component<void, IBlogState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            article: '',
+            articleBody: '',
             articleDefaultStyle: {
                 alpha: 0,
                 y: -200
             },
+            articleHeading: '',
             articleStyle: {
                 alpha: 0,
                 y: -200
             },
-            header: '',
             willAnimateInBlogLinkList: false
         };
     }
 
 //    Blog article component.
 
-    private index: any;
-
-    private blogLinkList: JSX.Element[];
-    private defaultStyles: Array<{ h: number }> = [];
+    private static blogLinkList: JSX.Element[];
+    private static defaultStyles: Array<{ h: number }> = [];
+    private static index: any;
 
     private async componentDidMount() {
         await this.fetchIndex();
@@ -81,35 +63,36 @@ export class Blog extends React.Component<void, IBlogState> {
             const indexRef = database.ref('index/');
             // Fetch index.
             indexRef.once('value').then((snapshot) => {
-                this.index = snapshot.val();
+                Blog.index = snapshot.val();
                 // Initialises blog entries.
-                this.blogLinkList = Object.entries(this.index).map(([key, value]) => (
+                Blog.blogLinkList = Object.entries(Blog.index).map(([key, value]) => (
                     <div
                         key={key}
-                        onClick={() => this.handleBlogLinkClick(key)}
+                        onClick={() => this.handleBlogLinkClick(value[`article`], value[`header`])}
                     >
                         {BlogLink(value[`header`], value[`tags`].split(','))}
                     </div>));
                 // Push style objects into array for StaggerMotion of Blog List.
-                this.blogLinkList.forEach(() => this.defaultStyles.push({h: -640}));
+                Blog.blogLinkList.forEach(() => Blog.defaultStyles.push({h: -640}));
                 // Fetch latest article using blog index length.
-                database.ref(this.index[this.index.length - 1][`article`].toString())
+                database.ref(Blog.index[Blog.index.length - 1][`article`].toString())
                     .once('value').then((articleSnapshot) =>
                     // Animate in latest article.
                     this.setState({
-                        article: articleSnapshot.val(), articleStyle: {
+                        articleBody: articleSnapshot.val(),
+                        articleHeading: Blog.index[Blog.index.length - 1][`header`],
+                        articleStyle: {
                             alpha: spring(1, presets.gentle),
                             y: spring(0, presets.gentle)
                         },
-                        header: this.index[this.index.length - 1][`header`],
                         willAnimateInBlogLinkList: true
                     }));
-            }).catch((err) => console.log(err));
+            }).catch((err) => this.setState({articleBody: err.message, articleHeading: err.name}));
             resolve();
         });
     }
 
-    private handleBlogLinkClick = async (key: string): Promise<any> => {
+    private handleBlogLinkClick = async (article: string, header: string): Promise<any> => {
         // Transition-out previous article.
         await new Promise((resolve) => {
             this.setState({
@@ -126,27 +109,21 @@ export class Blog extends React.Component<void, IBlogState> {
             window.setTimeout(() => resolve(), 500);
         });
         // Transition-in new article.
-        this.setState({
-            articleDefaultStyle: {
-                alpha: 0,
-                y: -200
-            },
-            articleStyle: {
-                alpha: spring(1, presets.gentle),
-                y: spring(0, presets.gentle)
-            }
-        });
-        (key === 'Cocaine') ?
+        this.setState({});
+        database.ref(article).once('value').then((snapshot) => {
             this.setState({
-                article: cocaineArticle.body.para1,
-                header: cocaineArticle.header,
-                willAnimateInBlogLinkList: true
-            }) :
-            this.setState({
-                article: methArticle.body.para1, header: methArticle.header, willAnimateInBlogLinkList: true
+                articleBody: snapshot.val(),
+                articleDefaultStyle: {
+                    alpha: 0,
+                    y: -200
+                },
+                articleHeading: header.toString(),
+                articleStyle: {
+                    alpha: spring(1, presets.gentle),
+                    y: spring(0, presets.gentle)
+                }
             });
-
-        // this.handleTagSort();
+        });
     };
 
     // private handleTagSort = (tag: string): any => {
@@ -181,7 +158,7 @@ export class Blog extends React.Component<void, IBlogState> {
                                 autoHideTimeout={1000}
                             >
                                 {this.state.willAnimateInBlogLinkList ? <StaggeredMotion
-                                    defaultStyles={this.defaultStyles}
+                                    defaultStyles={Blog.defaultStyles}
                                     styles={staggerStyles}
                                 >
                                     {(interpolatingStyles: object[]) =>
@@ -191,7 +168,7 @@ export class Blog extends React.Component<void, IBlogState> {
                                                     <div
                                                         style={{transform: `translateX(${style.h}px)`}}
                                                     >
-                                                        {this.blogLinkList[i]}
+                                                        {Blog.blogLinkList[i]}
                                                     </div>
                                                 </div>
                                             )}
@@ -202,24 +179,37 @@ export class Blog extends React.Component<void, IBlogState> {
                         </div>
                     </aside>
                     <main>
-                        <Motion
-                            defaultStyle={this.state.articleDefaultStyle}
-                            style={this.state.articleStyle}
-                        >
-                            {(interpolation: any) => <div
-                                className="blog-article"
-                                style={{
-                                    opacity: interpolation.alpha,
-                                    transform: `translateY(${interpolation.y}px)`
-                                }}
+                        <div className="blog-article">
+                            <Motion
+                                defaultStyle={this.state.articleDefaultStyle}
+                                style={this.state.articleStyle}
                             >
-                                <h1>{this.state.header}</h1>
-                                <div
-                                    className="blog-article-body"
-                                    dangerouslySetInnerHTML={{__html: this.state.article}}
-                                />
-                            </div>}
-                        </Motion>
+                                {(interpolation: any) => <div
+                                    style={{
+                                        opacity: interpolation.alpha,
+                                        transform: `translateX(${interpolation.y}px)`
+                                    }}
+                                >
+                                    <h1>{this.state.articleHeading}</h1>
+                                </div>}
+                            </Motion>
+                            <Motion
+                                defaultStyle={this.state.articleDefaultStyle}
+                                style={this.state.articleStyle}
+                            >
+                                {(interpolation: any) => <div
+                                    style={{
+                                        opacity: interpolation.alpha,
+                                        transform: `translateY(${interpolation.y}px)`
+                                    }}
+                                >
+                                    <div
+                                        className="blog-article-body"
+                                        dangerouslySetInnerHTML={{__html: this.state.articleBody}}
+                                    />
+                                </div>}
+                            </Motion>
+                        </div>
                     </main>
                 </div>
                 {NavMenu(inactive, active, inactive, inactive)}
