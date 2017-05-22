1eaf3306b5a899379d6514da9b29c0101877d811
r3b311i0n <nasashinega@gmail.com>
2017-05-22 13:10:56 +0530

    Start blog component markdown refactor.

--- src/app/stateless-components/nav-btn.tsx
+++ src/app/stateless-components/nav-btn.tsx
@@ -4,6 +4,8 @@ import './nav-btn.pcss';
 
 // Stateless nav-btn component that takes btn label and route destination, then puts them in a ReactRouterDom.NavLink.
 
+// todo: Have nav-btn emerge out from the page.
+
 const NavBtn = (btnLabel: string, emphasis: object, routeDestination: string) => (
     <NavLink to={routeDestination} className="nav-btn-root" style={emphasis}>{btnLabel}</NavLink>
 );
