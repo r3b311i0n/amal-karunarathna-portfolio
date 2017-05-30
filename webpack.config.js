c2200f1ef8fb2cb03d7ad65e33df58f4b53e5fb0
r3b311i0n <nasashinega@gmail.com>
2017-05-30 19:52:41 +0530

    Add prod webpack config. Find uglifyJS es6 issue.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = function (env) {
-    if (env !== 'prod') {
+    if (env === 'dev') {
         return {
             context: path.resolve(__dirname, './'),
 
@@ -85,14 +85,91 @@ module.exports = function (env) {
                     path.join(__dirname, 'public'),
                     path.join(__dirname, 'node_modules/react/dist'),
                     path.join(__dirname, 'node_modules/react-dom/dist'),
-                    path.join(__dirname, 'node_modules/redux/dist'),
-                    path.join(__dirname, 'node_modules/react-redux/dist'),
                     path.join(__dirname, 'node_modules/firebase')
                 ],
                 compress: true,
                 port: 4200,
                 publicPath: '/dist/',
                 historyApiFallback: true
+            }
+        };
+    }
+    else if (env === 'prod') {
+        return {
+            context: path.resolve(__dirname, './'),
+
+            entry: './src/index.tsx',
+            output: {
+                filename: 'bundle.js',
+                path: path.resolve(__dirname, 'dist/prod'),
+                publicPath: '/dist/prod/'
+            },
+
+            resolve: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx', '.css', '.pcss']
+            },
+
+            devtool: 'source-map',
+
+            module: {
+                rules: [
+                    {
+                        enforce: 'pre',
+                        test: /\.js$/,
+                        loader: 'source-map-loader',
+                        exclude: /node_modules/
+                    },
+                    {
+                        enforce: 'pre',
+                        test: /\.tsx?$/,
+                        use: 'source-map-loader',
+                        exclude: /node_modules/
+                    },
+                    {
+                        test: /\.tsx?$/,
+                        loader: 'awesome-typescript-loader',
+                        exclude: /node_modules/
+                    },
+                    {
+                        test: /\.pcss$/,
+                        use: ExtractTextPlugin.extract(
+                            {
+                                fallback: 'style-loader',
+                                use: [
+                                    {
+                                        loader: 'css-loader',
+                                        options: {
+                                            importLoaders: 1,
+                                            sourceMap: true
+                                        }
+                                    },
+                                    {
+                                        loader: 'postcss-loader',
+                                        options: {
+                                            config: {
+                                                ctx: {
+                                                    cssnext: {}
+                                                }
+                                            },
+                                            sourceMap: true
+                                        }
+                                    }
+                                ]
+                            }
+                        ),
+                        exclude: /node_modules/
+                    }
+                ]
+            },
+
+            plugins: [
+                new ExtractTextPlugin('styles.css')
+            ],
+
+            externals: {
+                'react': 'React',
+                'react-dom': 'ReactDOM',
+                'firebase': 'firebase'
             }
         };
     }
