f23b69a2685a22e832941ab772f1fab5129490eb
r3b311i0n <nasashinega@gmail.com>
2017-07-17 04:20:19 +0530

    Adjust Disqus component styles :speech_balloon:

--- src/app/disqus.component.tsx
+++ src/app/disqus.component.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import DisqusThread from 'react-disqus-comments';
+import './disqus.component.pcss';
 
 export interface IDisqusProps {
     id: string;
@@ -14,11 +15,13 @@ export class Disqus extends React.Component <IDisqusProps, {}> {
 
     public render(): JSX.Element {
         return (
-            <DisqusThread
-                shortname="r3b311i0n"
-                identifier={this.props.id}
-                title={this.props.title}
-            />
+            <div className="disqus-root">
+                <DisqusThread
+                    shortname="r3b311i0n"
+                    identifier={this.props.id}
+                    title={this.props.title}
+                />
+            </div>
         );
     }
 }
