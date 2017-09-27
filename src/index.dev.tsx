20bf8a67b230d7037a80002bd94393b126d78b38
Amal Karunarathna <nasashinega@gmail.com>
2017-09-27 08:46:18 +0530

    Remove Preact

--- src/index.dev.tsx
+++ src/index.dev.tsx
@@ -1,4 +1,3 @@
-import 'preact/devtools';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {BrowserRouter as BrowserRouter, Switch as Switch} from 'react-router-dom';
