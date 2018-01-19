f7a779bd233211ebb8a9848860cdafb29bd06ef7
Amal Karunarathna <nasashinega@gmail.com>
2018-01-19 20:05:41 +0530

    Add license to source :copyright: 🗎 Add Kotlin to about-me.tsx

--- src/app/about.component.tsx
+++ src/app/about.component.tsx
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 Amal Shasthree Karunarathna
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import * as firebase from 'firebase';
 import * as React from 'react';
 import {Motion, OpaqueConfig, presets, spring} from 'react-motion';
@@ -31,16 +55,17 @@ export class About extends React.Component<{}, IAboutState> {
     private static currTV: string;
 
     public componentDidMount() {
-        meRef.once('value').then((snapshot) => {
-            About.currAnime = snapshot.child('anime').val();
-            About.currBook = snapshot.child('book').val();
-            About.currGame = snapshot.child('game').val();
-            About.currMusic = snapshot.child('music').val();
-            About.currTV = snapshot.child('tv').val();
+        meRef.once('value')
+            .then((snapshot) => {
+                About.currAnime = snapshot.child('anime').val();
+                About.currBook = snapshot.child('book').val();
+                About.currGame = snapshot.child('game').val();
+                About.currMusic = snapshot.child('music').val();
+                About.currTV = snapshot.child('tv').val();
 
-            // For triggering the alpha animation for AboutMe.
-            this.setState({aboutMeMotion: {alpha: spring(1, presets.gentle)}});
-        });
+                // For triggering the alpha animation for AboutMe.
+                this.setState({aboutMeMotion: {alpha: spring(1, presets.gentle)}});
+            });
     }
 
     // Component of ME!!!.
